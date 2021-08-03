1d08e22933cbcd7177301a2a8f9df4b9d8192d05
Jonathan Goley <jgoley@gmail.com>
2021-08-03 14:27:58 -0400

    Cleanup action tab component

--- src/components/pages/map/action-tab/component.jsx
+++ src/components/pages/map/action-tab/component.jsx
@@ -1,50 +1,16 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
 // components
 import Filters from 'components/action-tab/filters';
 
-// constants
-
-class ActionTab extends PureComponent {
-
-  componentWillMount() {
-    const {
-      setFilters,
-      setTabFilters,
-      filters={},
-      tabFilters={},
-    } = this.props;
-  }
-
-  toggleModal(children) {
-    const { toggleModal } = this.props;
-
-    toggleModal(true, {
-      children,
-      size: '-auto'
-    });
-  }
-
-  render() {
-    return (
-      <div className="l-action">
-        <div className="l-filters">
-          <Filters name='Prioritize Basins' />
-        </div>
+const ActionTab = () => {
+  return (
+    <div className="l-action">
+      <div className="l-filters">
+        <Filters />
       </div>
-    );
-  }
-}
-
-ActionTab.propTypes = {
-  filters: PropTypes.object.isRequired,
-  tabFilters: PropTypes.object.isRequired,
-  setFilters: PropTypes.func.isRequired,
-  setTabFilters: PropTypes.func.isRequired,
-  toggleModal: PropTypes.func.isRequired
+    </div>
+  );
 };
-
-ActionTab.defaultProps = { geoStore: null };
 
 export default ActionTab;
