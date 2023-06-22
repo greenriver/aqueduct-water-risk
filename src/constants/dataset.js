79c6deaa356fd4e765d672a6821066c82d59a123
Carlos Del Real <carloshdelreal@gmail.com>
2023-06-22 13:57:55 -0500

    fix: revert datasets

--- src/constants/dataset.js
+++ src/constants/dataset.js
@@ -11,4 +11,19 @@ export const DATASET_DICTIONARY = {
   hydrobasins: '14a65097-974b-4084-932d-c7bdc5fe70f3',
   aquifers: 'd491f094-ad6e-4015-b248-1d1cd83667fa'
 };
+
+// export const DATASET_DICTIONARY = {
+//   // baseline datasets
+//   annual: '86d2a776-8744-468d-a66f-77f8d05d06a8',
+//   monthly: '7665cb4d-34ff-4a2d-a3bc-f59246d58578',
+//   // non-baseline datasets
+//   projected: '851b8ad1-1d80-46a7-b71a-e9dfa062f7b0',
+//   // presets
+//   weights: '605288fd-b146-480a-9098-3d2f46f2c403',
+//   custom: 'b318381e-485d-46c9-8958-c9a9d75d7e91',
+//   threshold: '5152c286-53c1-4583-9519-816a6e41889d',
+//   hydrobasins: '14a65097-974b-4084-932d-c7bdc5fe70f3',
+//   aquifers: 'd491f094-ad6e-4015-b248-1d1cd83667fa'
+// };
+
 export default { DATASET_DICTIONARY };
